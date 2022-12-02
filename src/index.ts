aa1abd7e8db3c92b2b34eeb19dd2d00fea421d5b
NeilKleistGao <neilkleist@gmail.com>
2022-12-02 12:10:44 +0800

    Add test

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
+import * as ts from "typescript";
+import { Transformer } from "./transformer";
 
+export default(p_program: ts.Program): ts.TransformerFactory<ts.Node> =>
+  (p_ctx) => {
+    const checker = p_program.getTypeChecker();
+    const transformer = new Transformer(); // TODO:
+    return (p_sf) => {
+      return transformer.run(p_sf as ts.SourceFile);
+    };
+  }
--- src/transformer.ts
+++ src/transformer.ts
@@ -1,1 +1,7 @@
+import * as ts from "typescript";
 
+export class Transformer {
+  run(p_sf: ts.SourceFile): ts.SourceFile {
+    return p_sf; // TODO:
+  }
+}
